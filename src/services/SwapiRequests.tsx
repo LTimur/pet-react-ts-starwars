c6822b5f3a806a46c624a9b3b062df31827cd9fb
LTimur <ledenev-timur@ya.ru>
2023-08-07 22:21:26 +0500

    add getplanets and getfilms fetch functions

--- src/services/SwapiRequests.tsx
+++ src/services/SwapiRequests.tsx
@@ -1,9 +1,39 @@
 export const baseUrl = "https://swapi.dev/api/";
 export const peopleUrl = `${baseUrl}/people`;
+export const filmsUrl = `${baseUrl}/films`;
+export const planetsUrl = `${baseUrl}/planets`;
 
 export const getCharacters = async (): Promise<any> => {
   try {
     const response = await fetch(peopleUrl);
+    if (response.ok) {
+      const jsonResponse = await response.json();
+      return jsonResponse;
+    }
+    throw new Error(`Can not get ${url}`);
+  } catch (error) {
+    console.error(error);
+    return error;
+  }
+};
+
+export const getFilms = async (): Promise<any> => {
+  try {
+    const response = await fetch(filmsUrl);
+    if (response.ok) {
+      const jsonResponse = await response.json();
+      return jsonResponse;
+    }
+    throw new Error(`Can not get ${url}`);
+  } catch (error) {
+    console.error(error);
+    return error;
+  }
+};
+
+export const getPlanets = async (): Promise<any> => {
+  try {
+    const response = await fetch(planetsUrl);
     if (response.ok) {
       const jsonResponse = await response.json();
       return jsonResponse;
