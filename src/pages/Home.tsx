63c1e97534230f925bd10df4e33b51908a8093c8
LTimur <ledenev-timur@ya.ru>
2023-08-08 19:30:31 +0500

    refactor home component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,18 +1,6 @@
 import {Container} from "../components/Container";
 import {Header} from "../components/Header";
 import styled from "styled-components";
-import logo from "../assets/logo.png";
-
-const LogoContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: flex-start;
-`;
-
-const Logo = styled.img`
-  width: 150px;
-  height: auto;
-`;
 
 const HomeContainer = styled.div`
   background-color: white;
