504a466b55de98eac8a6ade28492d038df92e7b5
LTimur <ledenev-timur@ya.ru>
2023-08-10 20:00:52 +0500

    update store

--- src/store.ts
+++ src/store.ts
@@ -1,8 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { charactersReducer } from "./features/CharactersSlice";
+import { filmsReducer } from "./features/FilmsSlice";
+import { planetsReducer } from "./features/PlanetsSlice";
 
 export const store = configureStore({
   reducer: {
     characters: charactersReducer,
+    films: filmsReducer,
+    planets: planetsReducer,
   },
 });
