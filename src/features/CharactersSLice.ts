541ea2dc7e0d91bc883d1eec6d1de9d8f978aae5
LTimur <ledenev-timur@ya.ru>
2023-08-10 16:55:12 +0500

    refactor charactersslice

--- src/features/CharactersSLice.ts
+++ src/features/CharactersSLice.ts
@@ -4,6 +4,16 @@ import type { PayloadAction } from "@reduxjs/toolkit";
 type StarWarsCharacter = {
   name: string;
   url: string;
+  height: string;
+  mass: string;
+  hair_color: string;
+  skin_color: string;
+  eye_color: string;
+  birth_year: string;
+  gender: string;
+  homeworld: string;
+  films: [];
+  
 };
 
 const initialState: StarWarsCharacter[] = [];
