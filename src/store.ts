71eaec43ca31d67d3e4d1d9317e85c66ee165959
LTimur <ledenev-timur@ya.ru>
2023-08-12 13:29:59 +0500

    add film and planet reducers to store

--- src/store.ts
+++ src/store.ts
@@ -2,13 +2,17 @@ import { configureStore } from "@reduxjs/toolkit";
 import { charactersReducer } from "./features/CharactersSlice";
 import { characterReducer } from "./features/CharacterSlice";
 import { filmsReducer } from "./features/FilmsSlice";
+import { filmReducer } from "./features/FilmSlice";
 import { planetsReducer } from "./features/PlanetsSlice";
+import { planetReducer } from "./features/PlanetSlice";
 
 export const store = configureStore({
   reducer: {
     characters: charactersReducer,
     character: characterReducer,
     films: filmsReducer,
+    film: filmReducer,
     planets: planetsReducer,
+    planet: planetReducer
   },
 });
