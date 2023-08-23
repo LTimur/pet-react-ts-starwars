e9a2443718f38309c650538771d35e9aa553dcf7
LTimur <ledenev-timur@ya.ru>
2023-08-23 19:20:20 +0500

    feat: add router in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { App } from './App.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/films',
+    element: <Films />,
+  },
+  {
+    path: '/films/:filmId',
+    element: <FilmCard />,
+  },
+  {
+    path: '/planets',
+    element: <Planets />,
+  },
+  {
+    path: '/planets/:planetId',
+    element: <PlanetCard />,
+  },
+  {
+    path: '/characters',
+    element: <Characters />,
+  },
+  {
+    path: '/characters/:characterId',
+    element: <CharacterCard />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
